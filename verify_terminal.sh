c7022f90dd3bfe08868f3b8055997c5048972164
pastorismylord <53154863+pastorismylord@users.noreply.github.com>
2021-09-21 16:29:45 -0500

    verify variables tool

---
+++ verify_terminal.sh
@@ -0,0 +1,6 @@
+echo boundary address: $BOUNDARY_ADDR
+echo Consull address: $CONSUL_HTTP_ADDR
+echo Consul token: $CONSUL_HTTP_TOKEN
+echo Vault address: $VAULT_ADDR
+echo Vault token: $VAULT_TOKEN
+echo Vault NameSpace: $VAULT_NAMESPACE
