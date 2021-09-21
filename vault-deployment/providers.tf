e1d7972574b08f086ceac0f0c4187796f1231dd6
pastorismylord <53154863+pastorismylord@users.noreply.github.com>
2021-09-21 13:24:08 -0500

    Revert "change on terraform org name"
    This reverts commit acb4a833b4a72aabf087cc01bd42d53edb35db64.

--- vault-deployment/providers.tf
+++ vault-deployment/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = "~> 1.0"
   backend "remote" {
-		organization = "edriosv-org" # org name from step 2.
+		organization = "EdV" # org name from step 2.
 		workspaces {
 		          	name = "vault-deployment" # name for your app's state.
 		          }
